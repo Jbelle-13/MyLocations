45dd409bb10b6984259c1f7ff0274bce7e2bf080
Jonathon Belliveau <Greenmantwo2@yahoo.com>
2020-09-27 18:42:02 -0400

    Initial Commit

---
+++ MyLocations/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  MyLocations
+//
+//  Created by Jeannine Roxas on 9/27/20.
+//  Copyright © 2020 Jonathon Belliveau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
